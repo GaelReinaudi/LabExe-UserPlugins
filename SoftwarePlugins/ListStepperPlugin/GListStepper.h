ed638ae7738af1df77bba6b773334ec6c990b963
GaelReinaudi <gael.reinaudi@gmail.com>
2025-03-16 18:29:03 -0400

    list stepper

---
+++ SoftwarePlugins/ListStepperPlugin/GListStepper.h
@@ -0,0 +1,75 @@
+#ifndef GListStepper_H
+#define GListStepper_H
+
+#include "device.h"
+#include "param.h"
+
+/////////////////////////////////////////////////////////////////////
+//! \brief The GListStepper class implements a GProgDevice.
+/*!
+This soft device allows you to define an input parameter by drag-and-dropping it in a param-bucket.
+When the input is updated, the device outputs the next value from a user-defined list.
+The list of values is specified in a text box and can be comma, space, or newline separated.
+The reset button restarts from the beginning of the list.
+*/
+//[inheritance]
+class GListStepper : public GProgDevice
+//[inheritance]
+{
+    Q_OBJECT
+
+public:
+
+//[constructor]
+    //! Constructor
+    GListStepper(QObject *parent, QString uniqueIdentifierName = "");
+//[constructor]
+    ~GListStepper();
+
+public slots:
+    //! Makes the stepper output the next value in the list
+    void StartUpdateOutput();
+    void UpdateOutput();
+    //! Resets the stepper to the first value in the list
+    void Reset();
+    //! Updates the value list from text input
+    void UpdateValueList();
+
+//![PopulateDeviceWidget]
+protected:
+    //! Re-implemented to provide a new widget adequately connected to this GListStepper.
+    void PopulateDeviceWidget(GDeviceWidget* theDeviceWidget);
+//![PopulateDeviceWidget]
+
+//! [variables]
+private:
+//! [variablesInputBucket]
+    //! the bucket for the input parameter
+    GSingleParamBucket m_InputBucket;
+    GSingleParamBucket m_OutputBucket;
+//! [variablesInputBucket]
+//! [variablesReset]
+    //! a reset boolean that will provide a reset button
+    GParamBool m_Reset;
+    GParamBool m_Enable;
+//! [variablesReset]
+//! [variablesOutput]
+    //! Values list text entry
+    GParamString m_ValueListText;
+    //! Time delay between trigger and output
+    GParamDouble m_Delay;
+    //! Current index in the list
+    GParamInt m_CurrentIndex;
+    //! Total number of values in the list
+    GParamInt m_ValuesCount;
+//! [variablesOutput]
+    //! List of values parsed from text input
+    QList<double> m_ValueList;
+//! [variables]
+
+private slots:
+    //! Called when the value list text is changed
+    void ParseValueList(const QString& text);
+};
+
+#endif // GListStepper_H 
---
+++ SoftwarePlugins/ListStepperPlugin/ListStepperPlugin.h
@@ -0,0 +1,31 @@
+#ifndef LISTSTEPPERPLUGIN_H
+#define LISTSTEPPERPLUGIN_H
+
+#include "ListStepperPlugin_global.h"
+#include "device.h"
+#include "GListStepper.h"
+
+/////////////////////////////////////////////////////////////////////
+//! \brief The GListStepperPlugin class implements a plugin that provides a stepper through a list of user-defined values.
+/*!
+This plugin allows you to define a list of values to step through in a text box.
+When the input bucket receives a trigger, it outputs the next value in the list.
+The list of values is specified in a text box and can be comma, space, or newline separated.
+The reset button restarts from the beginning of the list.
+*/
+class LISTSTEPPERPLUGINSHARED_EXPORT GListStepperPlugin : public GDevicePlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "Gael.GDevicePlugin/2.0")
+    Q_INTERFACES(GDevicePlugin)
+    G_REGISTER_PLUGIN_PROG_DEVICE(GListStepper)
+
+public:
+    GListStepperPlugin(QObject *parent = 0);
+    ~GListStepperPlugin();
+
+private:
+
+};
+
+#endif // LISTSTEPPERPLUGIN_H 
